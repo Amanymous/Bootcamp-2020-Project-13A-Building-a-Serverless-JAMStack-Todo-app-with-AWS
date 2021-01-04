46696a37933ffaf779fdf88ee7339346b4a4c98a
amanymous <aman.mirza9@yahoo.com>
2021-01-05 01:37:32 +0500

    cdk deploy done and deleted from s3

--- class-2-lambda/lib/class-2-lambda-stack.ts
+++ class-2-lambda/lib/class-2-lambda-stack.ts
@@ -9,7 +9,7 @@ export class Class2LambdaStack extends cdk.Stack {
     // The code that defines your stack goes here
     const hello = new lambda.Function(this,"HelloHandler",{
       runtime:lambda.Runtime.NODEJS_10_X,
-      code:lambda.Code.fromAsset("lambd"),
+      code:lambda.Code.fromAsset("./lambda"),
       handler:"hello.handler"
     })
     new apigw.LambdaRestApi(this,"Endpoint",{
