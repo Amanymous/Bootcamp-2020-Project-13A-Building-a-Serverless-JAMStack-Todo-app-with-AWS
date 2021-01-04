706e2c897ef5f01ab669ec4ee73c3bab6083f463
amanymous <aman.mirza9@yahoo.com>
2021-01-04 18:56:24 +0500

    deploy s3 bucket and delete it to check deployment

--- cdk-class1/bin/cdk-class1.ts
+++ cdk-class1/bin/cdk-class1.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkClass1Stack } from '../lib/cdk-class1-stack';
+import { CdkClassStack } from '../lib/cdk-class1-stack';
 
 const app = new cdk.App();
-new CdkClass1Stack(app, 'CdkClass1Stack');
+new CdkClassStack(app, 'CdkClassStack');
+
